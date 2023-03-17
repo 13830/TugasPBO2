d188e7a5a9e2be1b19f364b190f35c4f7b55d6d2
Arif Viansyah <127389870+13830@users.noreply.github.com>
2023-03-17 15:53:05 +0700

    Add files via upload

---
+++ Sepeda.java
@@ -0,0 +1,17 @@
+public class Sepeda {
+    int gear=5;
+    String jenis,merk;
+    
+    // konstruktor
+    //Sepeda(String merk) {
+    //  gear = 5;
+    //    this.merek = merk;
+    
+    
+    // method
+	
+
+    void ngerem() {
+        System.out.println("Sepeda Motor dapat direm");
+    }
+}
